975832f920e83a7011f1c5028ab8c6bad0b42a7e
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-03-02 17:03:48 +0300

    Create the pomodoro timer function

--- app/main/views.py
+++ app/main/views.py
@@ -22,3 +22,41 @@ def getTodos():
         todos.sort(key = lambda x: x.completed, reverse = True)
     
         return todos
+
+@main.route('/pomodoro', methods = ['GET', 'POST'])
+def pomodoro():
+    if request.method == 'POST':
+        inputTimerTarget = request.form['pomodoroInterval']
+        inputBreakTarget = request.form['breakInterval']
+
+        # Converyting tmes to minutes
+        timeTarget = inputTimerTarget[3:]
+        breakTarget = inputBreakTarget[3:]
+
+        newTimerDetails = Timer(username = current_user.username, pomodoro_interval = timeTarget,
+        break_interval = breakTarget)
+
+        db.session.add(newTimerDetails)
+        db.session.commit()
+
+        todos = getTodos()
+
+        return render_template('pomodoro.html', todos = todos, timeTarget = timeTarget, breakTarget = breakTarget,
+        inputTimerTarget = inputTimerTarget, inputBreakTarget = inputBreakTarget)
+
+    else:
+        timerDetails = Timer.query.filter_by(username = current_user.username).first()
+        todos = getTodos()
+        if(timerDetails):
+            inputTimerTarget = ('00:' + str(timerDetails.pomodoro_interval))
+            inputBreakTarget = ('00:' + str(timerDetails.break_interval))
+
+            return render_template('pomodoro.html', todo = todos, timerTarget = timerDetails.pomodoro_interval, breakTarget = timerDetails.break_interval,
+            inputTimerTarget = inputTimerTarget, inputBreakTarget = inputBreakTarget)
+
+        else:
+            timerTarget = '25'
+            breakTarget = '5'
+
+            return render_template('pomodoro.html', todos = todos, timerTarget = timerTarget, breakTarget = breakTarget,
+            inputTimerTarget = timerTarget, inputBreakTarget = breakTarget)
