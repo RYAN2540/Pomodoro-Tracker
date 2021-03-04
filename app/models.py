79234278893422e8aaaad46afaff0eee91988e01
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-03-04 12:22:32 +0300

    Change the error

--- app/models.py
+++ app/models.py
@@ -1,52 +1,18 @@
 from app import db
 from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
-from . import login_manager
+class Todos(db.Model):
+    id = db.Column(db.Integer(), primary_key = True)
+    category = db.Column(db.String(50), nullable = True)
+    description = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean(), default=False)
+    create_date = db.Column(db.DateTime(), default=datetime.now())
 
+class Timer(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    pomodoro_interval = db.Column(db.Integer())
+    break_interval = db.Column(db.Integer())
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-class User(UserMixin,db.Model): #db.Model helps connect our class to our database
-    __tablename__ = 'users' #gives tables in our database proper names
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique = True,index = True)
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
-    pass_secure = db.Column(db.String(255))
-    tasks = db.relationship('Task', backref='user',lazy="dynamic")
-
-    @property
-    def password(self):
-            raise AttributeError('You cannot read the password attribute') #raises an attribute error when we try to access the password
-
-    @password.setter
-    def password(self, password):
-            self.pass_secure = generate_password_hash(password)
-
-    def verify_password(self,password):
-            return check_password_hash(self.pass_secure,password)
-
-    def __repr__(self): #defines how the user object will be constructed when the class is called
-        return f'{self.username}'
-
-class Task(db.Model):
-    __tablename__ = 'tasks'
-    id = db.Column(db.Integer,primary_key = True)
-    task_title = db.Column(db.String())    
-    task_description = db.Column(db.String())
-    break_description = db.Column(db.String())
-    task_duration = db.Column(db.Integer())
-    break_duration = db.Column(db.Integer())
-    task_start_time = db.Column(db.DateTime,default=datetime.utcnow)    
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
-
-    def save_task(self):
-            db.session.add(self)
-            db.session.commit()
-
-    def __repr__(self):
-        return f'{self.task_title}'
+class Feedbacks(db.Model):
+	id = db.Column(db.Integer(), primary_key=True)
+	username = db.Column(db.String(50))
+	feedback = db.Column(db.String(100))
--- config.py
+++ config.py
@@ -7,14 +7,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///timer.db'
     SECRET_KEY = 'gakuyajefferson'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 class ProdConfig(Config):
     '''
@@ -33,9 +25,6 @@ class DevConfig(Config):
     '''
 
     pass
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ryan:crossman123@localhost/pomodorotracker'
-    
-    DEBUG = True
 
 class TestConfig(Config):
     '''
