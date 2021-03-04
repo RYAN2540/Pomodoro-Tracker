a6917dc8f6b24b4176c75ac13a04e9a6d12c9557
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-03-04 11:55:21 +0300

    Compare changes

--- app/models.py
+++ app/models.py
@@ -1,5 +1,9 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -46,21 +50,3 @@ class Task(db.Model):
 
     def __repr__(self):
         return f'{self.task_title}'
-
-
-class Todos(db.Model):
-    id = db.Column(db.Integer(), primary_key = True)
-    category = db.Column(db.String(50), nullable = True)
-    description = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Boolean(), default=False)
-    create_date = db.Column(db.DateTime(), default=datetime.now())
-
-class Timer(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    pomodoro_interval = db.Column(db.Integer())
-    break_interval = db.Column(db.Integer())
-
-class Feedbacks(db.Model):
-	id = db.Column(db.Integer(), primary_key=True)
-	username = db.Column(db.String(50))
-	feedback = db.Column(db.String(100))
--- manage.py
+++ manage.py
@@ -1,11 +1,6 @@
 from app import create_app, db
 from flask_script import Manager,Server
-<<<<<<< HEAD
 from flask_migrate import Migrate, MigrateCommand
-=======
-from app.models import User, Task
-from  flask_migrate import Migrate, MigrateCommand
->>>>>>> 3c575a9699105a90336370be2d0d4bb79b512854
 
 # Creating app instance
 app = create_app('development')
@@ -19,11 +14,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-<<<<<<< HEAD
     return dict(app = app, db = db)
-=======
-    return dict(app = app,db = db,User = User, Task=Task)
->>>>>>> 3c575a9699105a90336370be2d0d4bb79b512854
 
 if __name__ == '__main__':
     manager.run()
